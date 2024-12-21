7fe4dc9c3b4a4a16124b743a324b0655c7b2ee38
cybern00dle <koshech_noskich@mail.ru>
2024-12-21 13:52:55 +0300

    classwork tests

--- tests/test_classwork.py
+++ tests/test_classwork.py
@@ -10,7 +10,29 @@ class TestFiplPipl(unittest.TestCase):
     - граничное
     - тестирование состояния
     """
-    ...
+    def setUp(self):
+        self.fipl_pipl = FiplPipl(24)
+
+    def test_postpone_negative(self):
+        with self.assertRaises(ValueError):
+            self.fipl_pipl.postpone(-1)
+
+    def test_procrastinate_negative(self):
+        with self.assertRaises(ValueError):
+            self.fipl_pipl.procrastinate(-1)
+
+    def test_procrastinate_overflow(self):
+        with self.assertRaises(MissedDeadlineError):
+            self.fipl_pipl.procrastinate(25)
+
+    def test_postpone_positive(self):
+        self.assertEqual(self.fipl_pipl.postpone(5), 29)
+
+    def test_procrastinate_positive(self):
+        self.assertEqual(self.fipl_pipl.procrastinate(5), 19)
+
+    def test_check_deadline_positive(self):
+        self.assertEqual(self.fipl_pipl.check_deadline(), 24)
 
 
 if __name__ == '__main__':
