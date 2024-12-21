8e3df067552bf157c83f18e1ac77d8c95a291112
cybern00dle <koshech_noskich@mail.ru>
2024-12-21 13:31:54 +0300

    workflow added

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from src.trial_function import *
+from src.classwork import *
 
 
 if __name__ == '__main__':
     my_name = input('Hi! Please, tell me your name! You can *only* use latin letters, spaces hyphen and apostrophe.\n')
     print(say_hi(my_name))
+
+    fipl_pipl = FiplPipl(24)
+    # print(fipl_pipl.procrastinate(26))
